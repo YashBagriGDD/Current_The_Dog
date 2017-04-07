b8a9313c493c00fb928fe1909b272a7fa9d8290e
yxb1236 <yxb1236@GDD-19>
2017-04-07 17:44:35 -0400

    Started work on Catfish AI

--- Current/Current/Catfish.cs
+++ Current/Current/Catfish.cs
@@ -8,21 +8,44 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Current
 {
+    enum EnemyState {
+        Roaming,
+        PlayerDetected
+    }
+
     class Catfish : Enemy
     {
+        public EnemyState state { get; set; }
+        public bool IsAlive { get; set; }
+
         public Catfish(string name, Texture2D tex, Rectangle location) : base(name, tex, location)
         {
-            throw new NotImplementedException();
+            state = EnemyState.Roaming;
+            Health = 1;
+            IsAlive = true;
+            Speed = 3;
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            if (IsAlive == true) {
+                base.Draw(gameTime, spriteBatch);
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            //add update logic here
+            switch (state) {
+                case EnemyState.Roaming:
+                    break;
+                case EnemyState.PlayerDetected:
+                    break;
+                default:
+                    break;
+            }
+
+            base.Update(gameTime);
         }
 
         protected override void HandleCollisionEnter(object sender, EventArgs e)
